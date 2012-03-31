160280608520a50dce9d36c718d6c773c96e7195
Tatu Saloranta <tsaloranta@gmail.com>
2012-03-30 19:58:18 -0700

    ...

---
+++ src/test/java/com/fasterxml/jackson/dataformat/yaml/TestVersions.java
@@ -0,0 +1,44 @@
+package com.fasterxml.jackson.dataformat.yaml;
+
+import java.io.*;
+
+import com.fasterxml.jackson.core.*;
+
+public class TestVersions extends ModuleTestBase
+{
+    /**
+     * Not a good to do this, but has to do, for now...
+     */
+    private final static int MAJOR_VERSION = 2;
+    private final static int MINOR_VERSION = 0;
+
+    private final static String GROUP_ID = "com.fasterxml.jackson.dataformat";
+    private final static String ARTIFACT_ID = "jackson-dataformat-yaml";
+    
+    public void testMapperVersions() throws IOException
+    {
+        YAMLFactory f = new YAMLFactory();
+        assertVersion(f);
+        YAMLParser jp = (YAMLParser) f.createJsonParser("123");
+        assertVersion(jp);
+        YAMLGenerator jgen = (YAMLGenerator) f.createJsonGenerator(new ByteArrayOutputStream());
+        assertVersion(jgen);
+    }
+
+    /*
+    /**********************************************************
+    /* Helper methods
+    /**********************************************************
+     */
+    
+    private void assertVersion(Versioned vers)
+    {
+        final Version v = vers.version();
+        assertFalse("Should find version information (got "+v+")", v.isUknownVersion());
+        assertEquals(MAJOR_VERSION, v.getMajorVersion());
+        assertEquals(MINOR_VERSION, v.getMinorVersion());
+        assertEquals(GROUP_ID, v.getGroupId());
+        assertEquals(ARTIFACT_ID, v.getArtifactId());
+    }
+}
+
