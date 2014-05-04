7a1e51dda28e72552cf13cf74f3ae9f596d78dcc
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-05-04 12:43:46 -0700

    simplify test a bit

--- src/test/java/com/fasterxml/jackson/dataformat/yaml/failing/PolymorphicIdTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/yaml/failing/PolymorphicIdTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.yaml.ModuleTestBase;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -14,37 +13,17 @@ public class PolymorphicIdTest extends ModuleTestBase
         public Nested nested;
     }
 
-    public interface Nested {
-
-    }
-
-    @JsonTypeName("single")
-    static class NestedImpl implements Nested {
-
-    }
-
     @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
     @JsonSubTypes({ @JsonSubTypes.Type(value = NestedImpl.class) })
-    public class NestingMixin {
-    }
-    
-    static class TestModule extends SimpleModule {
+    public interface Nested { }
 
-        private static final long serialVersionUID = 1L;
+    @JsonTypeName("single")
+    static class NestedImpl implements Nested { }
 
-        @Override
-        public void setupModule(SetupContext context) {
-            setMixInAnnotation(Nested.class, NestingMixin.class);
-            super.setupModule(context);
-        }
-
-    }
-    
     @Test
     public void testPolymorphicType() throws Exception {
         final String YAML = "nested: \n    type: single\n";
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        mapper.registerModule(new TestModule());
         SingleNesting top = mapper.readValue(YAML, SingleNesting.class);
         assertNotNull(top);
     }
@@ -53,7 +32,6 @@ public class PolymorphicIdTest extends ModuleTestBase
     public void testNativePolymorphicType() throws Exception {
         String YAML = "nested: !single";
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        mapper.registerModule(new TestModule());
         SingleNesting top = mapper.readValue(YAML, SingleNesting.class);
         assertNotNull(top);
     }
