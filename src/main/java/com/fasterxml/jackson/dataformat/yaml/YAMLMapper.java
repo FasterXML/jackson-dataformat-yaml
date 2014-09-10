88e7fb3aca15f365d5bada6833b29aaa53e2ea0f
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-09-09 21:03:34 -0700

    Add `YAMLMapper` for convenience

---
+++ src/main/java/com/fasterxml/jackson/dataformat/yaml/YAMLMapper.java
@@ -0,0 +1,67 @@
+package com.fasterxml.jackson.dataformat.yaml;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Convenience version of {@link ObjectMapper} which is configured
+ * with {@lunk YAMLFactory}.
+ */
+public class YAMLMapper extends ObjectMapper
+{
+    private static final long serialVersionUID = 1L;
+
+    public YAMLMapper() { this(new YAMLFactory()); }
+
+    public YAMLMapper(YAMLFactory f) {
+        super(f);
+    }
+
+    /*
+    /**********************************************************************
+    /* Configuration
+    /**********************************************************************
+     */
+    
+    public YAMLMapper configure(YAMLGenerator.Feature f, boolean state) {
+        return state ? enable(f) : disable(f);
+    }
+
+    public YAMLMapper configure(YAMLParser.Feature f, boolean state) {
+        return state ? enable(f) : disable(f);
+    }
+
+    public YAMLMapper enable(YAMLGenerator.Feature f) {
+        ((YAMLFactory)_jsonFactory).enable(f);
+        return this;
+    }
+
+    public YAMLMapper enable(YAMLParser.Feature f) {
+        ((YAMLFactory)_jsonFactory).enable(f);
+        return this;
+    }
+
+    public YAMLMapper disable(YAMLGenerator.Feature f) {
+        ((YAMLFactory)_jsonFactory).disable(f);
+        return this;
+    }
+
+    public YAMLMapper disable(YAMLParser.Feature f) {
+        ((YAMLFactory)_jsonFactory).disable(f);
+        return this;
+    }
+
+    /*
+    /**********************************************************************
+    /* Additional typed accessors
+    /**********************************************************************
+     */
+
+    /**
+     * Overridden with more specific type, since factory we have
+     * is always of type {@link YAMLFactory}
+     */
+    @Override
+    public final YAMLFactory getFactory() {
+        return (YAMLFactory) _jsonFactory;
+    }
+}
