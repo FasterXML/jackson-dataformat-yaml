94ee4ace05ad1c75eb8cbd9cea7fcdcba09513ca
Benson Margulies <benson@basistech.com>
2014-11-16 19:03:29 -0500

    Fix issue #27. Don't try to use shade on the bundle. Make the shaded artifact a separate artifact with a classifier for those who need it. This could be reversed by making this a multi-module project and building the bundle separately.

---
+++ src/test/java/com/fasterxml/jackson/dataformat/yaml/OSGiIT.java
@@ -0,0 +1,59 @@
+/******************************************************************************
+ ** This data and information is proprietary to, and a valuable trade secret
+ ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ ** and may only be used as permitted under the license agreement under which
+ ** it has been distributed, and in no other way.
+ **
+ ** Copyright (c) 2014 Basis Technology Corporation All rights reserved.
+ **
+ ** The technical data and information provided herein are provided with
+ ** `limited rights', and the computer software provided herein is provided
+ ** with `restricted rights' as those terms are defined in DAR and ASPR
+ ** 7-104.9(a).
+ ******************************************************************************/
+
+package com.fasterxml.jackson.dataformat.yaml;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemPackages;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+/**
+ * Created by benson on 11/16/14.
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class OSGiIT {
+    @Configuration
+    public Option[] config() {
+        return options(
+                mavenBundle("com.fasterxml.jackson.core", "jackson-core").versionAsInProject(),
+                mavenBundle("com.fasterxml.jackson.core", "jackson-annotations").versionAsInProject(),
+                mavenBundle("com.fasterxml.jackson.core", "jackson-databind").versionAsInProject(),
+                mavenBundle("org.yaml", "snakeyaml").versionAsInProject(),
+                mavenBundle("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml").versionAsInProject(),
+                systemPackages(
+                        "javax.annotation"),
+                junitBundles(),
+                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN")
+        );
+    }
+
+    @Test
+    public void onceOver() throws Exception {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.writeValueAsString("Hello");
+    }
+}
